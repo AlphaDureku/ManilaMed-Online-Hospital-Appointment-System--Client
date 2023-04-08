8cec54af73560596429240a0270551b47637a0c6
AlphaDureku <templanzamark2002@gmail.com>
2023-04-09 03:17:26 +0800

    updated first page of booking

--- src/components/Booking Components/Body_Content/1st_Page.js
+++ src/components/Booking Components/Body_Content/1st_Page.js
@@ -2,20 +2,23 @@ export default function FirstPage() {
   return (
     <>
       <div className="FirstPage--body-wrapper">
-        <div>
+        <div className="Banner-wrapper">
           <img
             src="/images/1st_Page_Banner.png"
             className="Booking-banner"
           ></img>
         </div>
-        <div>
+        <div className="First_Page_Content">
           <h3>Set an Appointment</h3>
           <h5>Email Address</h5>
           <p>
             If you have previously made an online reservation, please supply
             your registered email address.
           </p>
-          <input></input>
+          <input
+            className="form-control First-page_input"
+            placeholder="Enter your email address"
+          ></input>
           <div>
             <input type="checkbox" id="terms"></input>
             <label for="terms">
@@ -24,7 +27,7 @@ export default function FirstPage() {
               <span style={{ color: "#2F9D44" }}>Terms & Conditions.</span>
             </label>
           </div>
-          <div className="1stPage--buttonRow">
+          <div className="FirstPage--buttonRow">
             <button>Back</button>
             <button>Proceed</button>
           </div>
