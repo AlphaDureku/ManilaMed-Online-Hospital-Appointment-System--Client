8ea133e6537c97c255f83ef7abc4bf085bad89d0
mrknoee <markfetero@gmail.com>
2023-04-17 18:56:35 +0800

    comment

--- src/components/Home Components/Hero Section/heroSection.js
+++ src/components/Home Components/Hero Section/heroSection.js
@@ -4,6 +4,7 @@ import { useMediaQuery } from "@mantine/hooks";
 function HeaderImg() {
   const isMobile = useMediaQuery("(max-width: 470px)");
 
+// HERO SECTION
   return (
     <div className="HeaderImg ">
       <div className="headerwave-container ">
