fa3226a815871b00cf475d0098a99b4beeee3cdf
AlphaDureku <templanzamark2002@gmail.com>
2023-05-23 04:58:36 +0800

    up

--- src/components/HeadAdmin_Components/Main_Content/login.js
+++ src/components/HeadAdmin_Components/Main_Content/login.js
@@ -12,7 +12,7 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-export default function Log() {
+export default function LoginPage() {
   const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
     username: "",
--- src/pages/HeadAdmin.js
+++ src/pages/HeadAdmin.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../components/HeadAdmin_Components/Main_Content/Dashboard";
-import Log from "../components/HeadAdmin_Components/Main_Content/Login";
+import LoginPage from "../components/HeadAdmin_Components/Main_Content/Login";
 export default function HeadAdmin() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Log />} />
+        <Route path="/" element={<LoginPage />} />
         <Route
           path="/dashboard"
           element={<Dashboard style={{ maxWidth: "1200px" }} />}
